44ae0dd5e40d25ef7837e34809c17f3d6f0877e0
ShreyaSingh-14 <86734942+Ash1412005Fame@users.noreply.github.com>
2025-07-31 11:55:40 +0530

    server added

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
-const http = require('http');
-http.createServer((req, res) => {
-res.writeHead(200, {'Content-Type': 'text/plain'});
-res.end("Hello World\n");
-}).listen(8080);
-console.log("Server running at http://localhost:8080/");
-// This code creates a simple HTTP server that listens on port 8080 and responds with "Hello World" when accessed.
+// const http = require('http');
+// http.createServer((req, res) => {
+// res.writeHead(200, {'Content-Type': 'text/plain'});
+// res.end("Hello World\n");
+// }).listen(8080);
+// console.log("Server running at http://localhost:8080/");
+// // This code creates a simple HTTP server that listens on port 8080 and responds with "Hello World" when accessed.
+const express=require('express');
+const path=require("path");
+
+const app=express();
+const PORT=process.env.PORT|| 3000;
+app.use(express.static(path.join(__dirname, 'Views')));
+
+app.get('/', (req, res) => {
+console.log(req)
+});
+//start the server
+app.listen(PORT, () => {   
+    console.log(`Server is running on http://localhost:${PORT}`);
+    }
+);
